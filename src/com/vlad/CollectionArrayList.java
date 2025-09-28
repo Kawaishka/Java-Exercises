16e302ac29bfd5507c7b8ed869e652fee1a9bcef
Amadeika <130800578+Kawaishka@users.noreply.github.com>
2025-09-28 23:30:34 +0200

    Restructure project: organize files into packages and rename classes

---
+++ src/com/vlad/CollectionArrayList.java
@@ -0,0 +1,26 @@
+package com.vlad;
+
+import java.util.ArrayList;
+
+public class CollectionArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(10);
+        numbers.add(200);
+        numbers.add(1, 99);
+
+
+
+        //System.out.println(numbers.get(1)); // get first element
+        numbers.remove(1); // remove first element
+
+        //numbers.clear(); // clear all elements
+
+        System.out.println("Size of collection: " + numbers.size());
+
+        for(Integer el : numbers) {
+            System.out.println(el);
+        }
+    }
+}
